667f67c08a66d3adb319108f22e093c30bba001f
James Liu <jamesjhliu@gmail.com>
2016-03-08 00:08:15 -0800

    update port for heroku

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var server = ws.createServer(conn => {
   console.log('Connection opened.');
   conn.on('text', str => console.log(str));
   conn.on('close', (code, reason) => console.log('Connection closed.'));
-}).listen(8080);
+}).listen(process.env.PORT || 8080);
